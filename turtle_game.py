b6bc0b8a722135ae3318075773f069e0d40780e7
dirkwind <dirkwind707@gmail.com>
2020-01-24 22:44:40 -0600

    reorganize turtle_game.py

--- turtle_game.py
+++ turtle_game.py
@@ -1,5 +1,25 @@
 import turtle, random, threading, time, sys
 
+# NOTE: The entire game is piled into one file because of the way I made functions; they depend on variables within this file.
+
+# TABLE OF CONTENTS:
+#   highlight a word and use cmd+f to navigate
+#
+# INTRO
+# SETUP
+# CLASSES
+# FUNCTIONS
+#   LEVEL
+#   GETTERS AND UPDATERS
+#       GETTERS
+#       UPDATERS
+#   LEADERBOARD
+#   CHECKERS
+#   MOVEMENT
+# INITIALIZING GAME
+# USER INPUT
+
+# -------------------------------------------- INTRO -------------------------------------------- #
 
 def text_scroll(z):
     for x in z:
@@ -75,7 +95,7 @@ else:
     score_increment = 5
     map_size = random.choice([10, 12, 14, 16, 18, 20])
 
-
+# -------------------------------------------- SETUP -------------------------------------------- #
 
 screen = turtle.Screen()
 
@@ -133,6 +153,8 @@ for multiplier in range(1, 100):
     multiples_of_dis.append(distance * -multiplier)
 multiples_of_dis.sort()
 
+
+# -------------------------------------------- CLASSES -------------------------------------------- #
 
 class Enemy(object):
 
@@ -215,7 +237,40 @@ class Pathfinder(object):
             new_pos = self.q_list.pop(0)
             self.check_neighbors(new_pos, distances)
         
-        
+
+# -------------------------------------------- FUNCTIONS -------------------------------------------- #
+
+# --------------------------- LEVEL --------------------------- #
+
+def change_level(level_increase, paths_bonus, score_bonus):
+    global gamemode, level, paths, score
+    gamemode = 'build'
+    score += score_bonus
+    paths = 0
+    paths += round(paths_bonus/path_handicap)
+    level += level_increase
+    level_turt.clear()
+    level_turt.write("Level: "+str(level), font=('Arial', 25, 'normal'))
+    path_turt.clear()
+    path_turt.write('Paths: ' + str(paths), font=('Arial', 25, 'normal'))
+
+def end_game():
+    global gamemode
+    screen.clear()
+    gamemode = 'dead'
+    add_user_leaderboard(difficulty, username, score)
+    get_leaderboard(difficulty)
+    score_turt.goto(-200, 0)
+    score_turt.clear()
+    score_turt.write("Your Score: {}".format(round(score)), font=('Arial', 50, 'normal'))
+    score_turt.goto(-200, -25)
+    score_turt.write("Level: " + str(level) + "\tDifficulty: " + difficulty.capitalize() + "", font=('Arial', 20, 'normal'))
+    score_turt.goto(1000,1000)
+
+# --------------------------- GETTERS AND UPDATERS --------------------------- #
+
+# ----- GETTERS ----- #
+
 def get_position(turtle):
     xcor = int(turtle.xcor())
     ycor = int(turtle.ycor())
@@ -229,14 +284,6 @@ def get_position(turtle):
         new_coords.append(multiples_of_dis[round_index])
     xcor, ycor = new_coords
     return (xcor, ycor)
-
-def check_path(prev_position):
-    global paths
-    if (get_position(player), prev_position) not in coords_visited and (prev_position, get_position(player)) not in coords_visited:
-        paths -= 1
-        coords_visited.add((get_position(player), prev_position))
-        coords_visited.add((prev_position, get_position(player)))
-        coords_in_play.add(get_position(player))
     
 def get_projected_path(direction, turtle):
     if direction == 'right':
@@ -253,7 +300,127 @@ def get_projected_path(direction, turtle):
         return (get_position(turtle), (get_position(turtle)[0], y))
     else:
         print('Error: unknown distance input (' + str(direction) + ') detected.')
-    
+
+# ----- UPDATERS ----- #
+
+def update_score():
+    global score, paths, refresh
+    if gamemode == 'run':
+        score += score_increment
+        if score == 400:
+            change_level(1, 10, 50)
+        elif score == 900:
+            change_level(1, 20, 100)
+        elif score == 1600:
+            change_level(1, 30, 150)
+        elif score == 2300:
+            change_level(1, 40, 200)
+            refresh = round(refresh/2)
+        elif score == 3100:
+            change_level(1, 30, 250)
+            enemy3 = Enemy('yellow', 1, 'square', 5)
+            enemy_list.append(enemy3)
+        elif score == 4200:
+            change_level(1, 20, 300)
+            center_player()
+            make_border(map_size - 2, 'orange')
+            refresh = round(refresh/2)
+        elif score == 5500:
+            change_level(1, 10, 350)
+        elif score == 6500:
+            change_level(1, 9, 400)
+            center_player()
+            make_border(map_size - 4, 'yellow')
+        elif score == 7600:
+            change_level(1, 5, 450)
+            refresh = round(refresh/2)
+        elif score == 9000:
+            change_level(1, 4, 500)
+            enemy4 = Enemy('purple', 1, 'turtle', 6)
+            enemy_list.append(enemy4)
+        elif score == 11000:
+            change_level(1, 3, 550)
+        elif score == 13000:
+            change_level(1, 2, 600)
+        elif score == 16200:
+            change_level(1, 1, 650)
+        elif score == 18000:
+            change_level(1, 1, 650)
+            center_player()
+            make_border(map_size - 6, 'blue')
+        elif score == 21000:
+            change_level(1, 0, 700)
+        elif score == 24000:
+            change_level(1, 0, 750)
+            refresh = round(refresh/4)
+        elif score == 29000:
+            change_level(1, 0, 800)
+        elif score == 35000:
+            change_level(1, 1, 850)
+            center_player()
+            make_border(map_size - 8, 'purple')
+        score_turt.clear()
+        score_turt.write(score, font=('Arial', 25, 'normal'))
+    screen.ontimer(update_score, refresh)
+
+
+def update_player_pos():
+    global player_coords
+    if gamemode == 'run':
+        for en in enemy_list:
+            en.choose_direction()
+        for en in enemy_list:
+            if abs(en.turtle.xcor() - player.xcor()) < 10 and abs(en.turtle.ycor() - player.ycor()) < 10:
+                end_game()
+    screen.ontimer(update_player_pos, refresh)
+
+# --------------------------- LEADERBOARD --------------------------- #
+
+def get_leaderboard(difficulty):
+    infile = open("saves/turtle_leaderboard.txt", 'r')
+    leaderboard = {}
+    index = 0
+    print('Difficulty User : Score')
+    for line in infile:
+        line = line.split('|')
+        leaderboard[str(line[0])+str(index).zfill(8)] = round(float(line[1]))
+        index += 1
+    while len(leaderboard) > 0:
+        highest = max(leaderboard, key=leaderboard.get)
+        highest_split = highest.split('!')
+        if highest_split[0] == 'n':
+            d = 'Normal'
+        elif highest_split[0] == 'e':
+            d = 'Easy'
+        elif highest_split[0] == 'h':
+            d = 'Hard'
+        elif highest_split[0] == 'i':
+            d = 'Impossible'
+        elif highest_split[0] == 'c':
+            d = 'Cheat'
+        elif highest_split[0] == 'r':
+            d = 'Random'
+        if d.lower() == difficulty.lower():
+            print(d, highest_split[1][:len(highest_split[1])-8], ":", leaderboard.pop(highest))
+        else:
+            leaderboard.pop(highest)
+    infile.close()
+
+def add_user_leaderboard(difficulty, username, score):
+    infile = open("saves/turtle_leaderboard.txt", 'a')
+    infile.write(difficulty.lower()[0]+'!'+str(username)+'|'+str(score)+'\n')
+    infile.close()
+
+# --------------------------- CHECKERS --------------------------- #
+
+def check_path(prev_position):
+    global paths
+    if (get_position(player), prev_position) not in coords_visited and (prev_position, get_position(player)) not in coords_visited:
+        paths -= 1
+        coords_visited.add((get_position(player), prev_position))
+        coords_visited.add((prev_position, get_position(player)))
+        coords_in_play.add(get_position(player))
+
 def check_drawing():
     global gamemode, enemy, enemy2, paths
     if paths == 0:
@@ -272,20 +439,17 @@ def check_drawing():
         player.pendown()
         path_turt.clear()
         path_turt.write('Paths: ' + str(paths), font=('Arial', 25, 'normal'))
-    
-def end_game():
-    global gamemode
-    screen.clear()
-    gamemode = 'dead'
-    add_user_leaderboard(difficulty, username, score)
-    get_leaderboard(difficulty)
-    score_turt.goto(-200, 0)
-    score_turt.clear()
-    score_turt.write("Your Score: {}".format(round(score)), font=('Arial', 50, 'normal'))
-    score_turt.goto(-200, -25)
-    score_turt.write("Level: " + str(level) + "\tDifficulty: " + difficulty.capitalize() + "", font=('Arial', 20, 'normal'))
-    score_turt.goto(1000,1000)
-    
+
+# --------------------------- MOVEMENT --------------------------- #
+
+def center_player():
+    global moving
+    moving = True
+    player.penup()
+    player.speed(0)
+    player.setpos(0, 0)
+    player.speed(speed)
+    moving = False
 
 def move_right():
     global moving
@@ -359,140 +523,21 @@ def move_down():
         moving = False
     check_drawing()
 
-def change_level(level_increase, paths_bonus, score_bonus):
-    global gamemode, level, paths, score
-    gamemode = 'build'
-    score += score_bonus
-    paths = 0
-    paths += round(paths_bonus/path_handicap)
-    level += level_increase
-    level_turt.clear()
-    level_turt.write("Level: "+str(level), font=('Arial', 25, 'normal'))
-    path_turt.clear()
-    path_turt.write('Paths: ' + str(paths), font=('Arial', 25, 'normal'))
 
-def center_player():
-    global moving
-    moving = True
-    player.penup()
-    player.speed(0)
-    player.setpos(0, 0)
-    player.speed(speed)
-    moving = False
-
-def update_score():
-    global score, paths, refresh
-    if gamemode == 'run':
-        score += score_increment
-        if score == 400:
-            change_level(1, 10, 50)
-        elif score == 900:
-            change_level(1, 20, 100)
-        elif score == 1600:
-            change_level(1, 30, 150)
-        elif score == 2300:
-            change_level(1, 40, 200)
-            refresh = round(refresh/2)
-        elif score == 3100:
-            change_level(1, 30, 250)
-            enemy3 = Enemy('yellow', 1, 'square', 5)
-            enemy_list.append(enemy3)
-        elif score == 4200:
-            change_level(1, 20, 300)
-            center_player()
-            make_border(map_size - 2, 'orange')
-            refresh = round(refresh/2)
-        elif score == 5500:
-            change_level(1, 10, 350)
-        elif score == 6500:
-            change_level(1, 9, 400)
-            center_player()
-            make_border(map_size - 4, 'yellow')
-        elif score == 7600:
-            change_level(1, 5, 450)
-            refresh = round(refresh/2)
-        elif score == 9000:
-            change_level(1, 4, 500)
-            enemy4 = Enemy('purple', 1, 'turtle', 6)
-            enemy_list.append(enemy4)
-        elif score == 11000:
-            change_level(1, 3, 550)
-        elif score == 13000:
-            change_level(1, 2, 600)
-        elif score == 16200:
-            change_level(1, 1, 650)
-        elif score == 18000:
-            change_level(1, 1, 650)
-            center_player()
-            make_border(map_size - 6, 'blue')
-        elif score == 21000:
-            change_level(1, 0, 700)
-        elif score == 24000:
-            change_level(1, 0, 750)
-            refresh = round(refresh/4)
-        elif score == 29000:
-            change_level(1, 0, 800)
-        elif score == 35000:
-            change_level(1, 1, 850)
-            center_player()
-            make_border(map_size - 8, 'purple')
-        score_turt.clear()
-        score_turt.write(score, font=('Arial', 25, 'normal'))
-    screen.ontimer(update_score, refresh)
-
-
-def update_player_pos():
-    global player_coords
-    if gamemode == 'run':
-        for en in enemy_list:
-            en.choose_direction()
-        for en in enemy_list:
-            if abs(en.turtle.xcor() - player.xcor()) < 10 and abs(en.turtle.ycor() - player.ycor()) < 10:
-                end_game()
-    screen.ontimer(update_player_pos, refresh)
-
-def get_leaderboard(difficulty):
-    infile = open("saves/turtle_leaderboard.txt", 'r')
-    leaderboard = {}
-    index = 0
-    print('Difficulty User : Score')
-    for line in infile:
-        line = line.split('|')
-        leaderboard[str(line[0])+str(index).zfill(8)] = round(float(line[1]))
-        index += 1
-    while len(leaderboard) > 0:
-        highest = max(leaderboard, key=leaderboard.get)
-        highest_split = highest.split('!')
-        if highest_split[0] == 'n':
-            d = 'Normal'
-        elif highest_split[0] == 'e':
-            d = 'Easy'
-        elif highest_split[0] == 'h':
-            d = 'Hard'
-        elif highest_split[0] == 'i':
-            d = 'Impossible'
-        elif highest_split[0] == 'c':
-            d = 'Cheat'
-        elif highest_split[0] == 'r':
-            d = 'Random'
-        if d.lower() == difficulty.lower():
-            print(d, highest_split[1][:len(highest_split[1])-8], ":", leaderboard.pop(highest))
-        else:
-            leaderboard.pop(highest)
-    infile.close()
-
-def add_user_leaderboard(difficulty, username, score):
-    infile = open("saves/turtle_leaderboard.txt", 'a')
-    infile.write(difficulty.lower()[0]+'!'+str(username)+'|'+str(score)+'\n')
-    infile.close()
-
+# -------------------------------------------- INITIALIZING GAME -------------------------------------------- #
 
 make_border(map_size, 'red')
 player = turtle.Turtle()
 player.speed(speed)
 pathfinder = Pathfinder()
-score_thread = threading.Thread(target=update_score)
-player_pos_thread = threading.Thread(target=update_player_pos)
+score_thread = threading.Thread(target=update_score) # subject for removal
+player_pos_thread = threading.Thread(target=update_player_pos) # subject for removal
+
+score_thread.start()
+player_pos_thread.start()
+level_turt.write("Level: "+str(level), font=('Arial', 25, 'normal'))
+
+# -------------------------------------------- USER INPUT -------------------------------------------- #
 
 screen.onkeypress(move_left, 'a')
 screen.onkeypress(move_right, 'd')
@@ -503,11 +548,9 @@ screen.onkeypress(move_right, 'Right')
 screen.onkeypress(move_up, 'Up')
 screen.onkeypress(move_down, 'Down')
 
-score_thread.start()
-player_pos_thread.start()
-level_turt.write("Level: "+str(level), font=('Arial', 25, 'normal'))
-
 screen.listen()
+
+# not related to USER INPUT
 screen.mainloop()
 
 
